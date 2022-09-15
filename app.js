3008a2c5c87d18768d84fc99ae174f99984b05d5
ionelabrinza <ionela.brinza@yahoo.com>
2022-09-14 21:56:28 -0500

    setting date in Js

--- app.js
+++ app.js
@@ -21,3 +21,21 @@ const weekdays = [
     "Friday",
     "Saturday",
 ];
+
+const giveaway = document.querySelector('.giveaway');
+const deadline = document.querySelector('.deadline');
+const items = document.querySelectorAll('.deadline-format h4');
+
+let futureDate = new Date(2023, 4, 24, 11, 30, 0);
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+
+let month = futureDate.getMonth();
+month = months[month];
+
+const date = futureDate.getDate();
+
+const weekday = weekdays[futureDate.getDay()];
+giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${month} ${year} ${hours}: ${minutes}am`;
