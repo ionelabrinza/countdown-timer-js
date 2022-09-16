ff823b0b9debc9ed2db73cec739de0b4cd118580
ionelabrinza <ionela.brinza@yahoo.com>
2022-09-15 22:36:24 -0500

    countdown setup dinamically

--- app.js
+++ app.js
@@ -28,6 +28,8 @@ const items = document.querySelectorAll('.deadline-format h4');
 
 let futureDate = new Date(2023, 4, 24, 11, 30, 0);
 
+
+
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
 const minutes = futureDate.getMinutes();
@@ -39,3 +41,34 @@ const date = futureDate.getDate();
 
 const weekday = weekdays[futureDate.getDay()];
 giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${month} ${year} ${hours}: ${minutes}am`;
+
+// future time in ms
+
+const futureTime = futureDate.getTime();
+
+function getRemainingTime() {
+    const today = new Date().getTime();
+    const t = futureTime - today;
+    // 1 s = 1000 ms
+    // 1 min = 60 s
+    // 1 hr = 60min
+    // 1 day = 24hours
+    // values in ms
+    const oneDay = 24 * 60 * 60 * 1000;
+    const oneHour = 60 * 60 * 1000;
+    const oneMinute = 60 * 1000;
+    let days = t / oneDay;
+    days = Math.floor(days);
+    let hours = Math.floor((t % oneDay) / oneHour);
+    let minutes = Math.floor((t % oneHour / oneMinute))
+    let seconds = Math.floor((t % oneMinute) / 1000)
+
+    //set values arrays
+
+    const values = [days, hours, minutes, seconds];
+
+    items.forEach(function(item, index) {
+        item.innerHTML = values[index]
+    });
+}
+getRemainingTime()
