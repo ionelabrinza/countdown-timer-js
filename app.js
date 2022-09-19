3dcd989c620917f8321f53039e4db6aae05b1d6a
ionelabrinza <ionela.brinza@yahoo.com>
2022-09-18 22:20:50 -0500

    last changes - adding future date alternative

--- app.js
+++ app.js
@@ -26,9 +26,14 @@ const giveaway = document.querySelector('.giveaway');
 const deadline = document.querySelector('.deadline');
 const items = document.querySelectorAll('.deadline-format h4');
 
-let futureDate = new Date(2023, 4, 24, 11, 30, 0);
+let tempDate = new Date();
+let tempYear = tempDate.getFullYear();
+let tempMonth = tempDate.getMonth();
+let tempDay = tempDate.getDate()
 
+// let futureDate = new Date(2022, 10, 10, 1, 30, 0);
 
+const futureDate = new Date(tempYear, tempMonth, tempDay + 10, 11, 30, 0);
 
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
@@ -77,7 +82,12 @@ function getRemainingTime() {
     items.forEach(function(item, index) {
         item.innerHTML = format(values[index]);
     });
+    if (t < 0) {
+        clearInterval(countdown);
+        deadline.innerHTML = `<h4> class="expired>sorry, this giveaway has expired</h4>`
+    }
 }
 // countdown
 let countdown = setInterval(getRemainingTime, 1000);
+
 getRemainingTime()
